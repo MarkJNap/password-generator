393178aad1170c0c6e750a79953c554cc7bba363
Mark Napolitano <156marknap@gmail.omgit config --global user.name oMarkNx>
2022-11-30 13:40:02 +0800

    removing testing comments and adding useful comments

--- js/script.js
+++ js/script.js
@@ -1,92 +1,70 @@
-// TODO Click button to start prompts
-// Length from 8 - 128 charcters
-// need to ask : lowercase, uppercase, numeric, special characters\
-// abcdefghijklmnopqrstuvwxyz
-// 0123456789
-// " !#$%&()*+,-./:;<=>?@[]^_{|}~"
-// then generate the password and display it
 
 const alphabet = "abcdefghijklmnopqrstuvwxyz"
 const number = "0123456789"
 const symbol = " !#$%&()*+,-./:;<=>?@[]^_{|}~"
 
-// Function for a random char for each type (started here might end up combining and then getting random chars)
-// function randomLower() {
-//   return alphabet[Math.floor(Math.random() * alphabet.length)]
-// }
-// function randomUpper() {
-//   return alphabet.toUpperCase()[Math.floor(Math.random() * alphabet.length)]
-// }
-// function randomNumber() {
-//   return number[Math.floor(Math.random() * number.length)]
-// }
-// function randomSymbol() {
-//   return symbol[Math.floor(Math.random() * symbol.length)]
-// }
-
-
-// console.log(randomLower());
-// console.log(randomUpper());
-// console.log(randomNumber());
-// console.log(randomSymbol());
-
-
-// Basic button press layout for testing interactions
+// Basic button press layout for any interactions
 var generatePassword = function() {
   var charsMax = ""
   var passLength = window.prompt("Please enter a length between 8 and 128");
+
   // If cancel is pressed on first box stops function
   if (!passLength) {
     return passwordText = "Press Generate Password for a new password";
   }
-
-  var genLower = window.confirm("Do you want lowercase letters?");
-  var genUpper = window.confirm("Do you want uppercase letters?");
-  var genNumber = window.confirm("Do you want numbers?");
-  var genSymbol = window.confirm("Do you want symbols?");
+  // sets var to true if confirmed
+  var lowerOption = window.confirm("Do you want lowercase letters?");
+  var upperOption = window.confirm("Do you want uppercase letters?");
+  var numberOption = window.confirm("Do you want numbers?");
+  var symbolOption = window.confirm("Do you want symbols?");
 
   // If cancel is pressed on all
-  if ((!genLower) && (!genUpper) && (!genNumber) && (!genSymbol)) {
+  if ((!lowerOption) && (!upperOption) && (!numberOption) && (!symbolOption)) {
     return passwordText = "Please pick a valid way to generate a password.";
   }
 
-
-  // To change passLength to a number
+  // To change passLength from a string to a number
   var passLengthNumber = (passLength * 1);
-  // breaks if not a number is entered TODO
-  if ((passLengthNumber < 7) || (passLengthNumber > 128)) {
+
+  // Stops the function if not a number and returns that text if it is
+  if ((passLengthNumber < 8) || (passLengthNumber > 128)) {
     return passwordText = "Please enter a valid number.";
   }
 
-  // Adds characters if they are true
-  if (genLower) {
+  // Adds characters together if they were true
+  if (lowerOption) {
     charsMax = charsMax + alphabet
   }  
-  if (genUpper) {
+  if (upperOption) {
     charsMax = charsMax + alphabet.toUpperCase()
   }  
-  if (genNumber) {
+  if (numberOption) {
     charsMax = charsMax + number
   }  
-  if (genSymbol) {
+  if (symbolOption) {
     charsMax = charsMax + symbol
   }
+
   // Empty string to fill with password
-  var passGeneration = "";
+  var passwordGeneration = "";
   for (var i = 0; i < passLengthNumber; i++) {
     var randomChar = charsMax[Math.floor(Math.random() * charsMax.length)]
-    passGeneration = passGeneration + randomChar
+    passwordGeneration = passwordGeneration + randomChar
   }
-  console.log(passGeneration);
-  return passwordText = passGeneration
+  // console.log(passGeneration);
+  return passwordText = passwordGeneration
 }
+
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
   passwordText.value = password;
 }
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+
